633a01181ad9736f8624a867946763b664a9719d
David R. Myers <davidrmyersii@gmail.com>
2025-06-29 20:12:20 -0400

    Add Cloudflare config (and deploy)

--- apps/default/astro.config.ts
+++ apps/default/astro.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwind from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [
+      cloudflare(),
       tailwind(),
     ],
   },
